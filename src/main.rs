0548bfbf7f4562c6aca663ef584d03faeb09864e
4BMtolobPL <k4qe6urml3@gmail.com>
2022-07-07 16:24:18 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ fn main() {
     std::io::stdin().read_line(&mut buf).unwrap();
     let v: Vec<i32> = buf.split_whitespace().map(|x| x.parse().unwrap()).collect();
 
-    let board: Vec<String> = Vec::new();
-    for i in 0..v[0] {
-        board.push(Vec::new());
+    let mut board: Vec<String> = Vec::new();
+    for _ in 0..v[0] {
         let mut buf = String::new();
         std::io::stdin().read_line(&mut buf).unwrap();
-        for c in buf.trim().chars() {
-            board[i].push(c);
-        }
+        board.push(buf.trim().to_string());
     };
 }
