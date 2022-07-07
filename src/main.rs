9b2d94c66756f5e1e10482d284f6e55ba1a92628
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2022-07-07 17:57:18 +0900

    1018

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-fn main() {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).unwrap();
-    let v: Vec<i32> = buf.split_whitespace().map(|x| x.parse().unwrap()).collect();
+mod problem;
+use problem::q1018;
 
-    let mut board: Vec<String> = Vec::new();
-    for _ in 0..v[0] {
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).unwrap();
-        board.push(buf.trim().to_string());
-    };
+fn main() {
+    q1018::solve();
 }
+
