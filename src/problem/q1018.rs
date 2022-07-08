dd970cd42074e052c11f934ce4a485d5a1d6cfe4
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2022-07-08 11:47:29 +0900

    q1018 solved
    Yes

--- src/problem/q1018.rs
+++ src/problem/q1018.rs
@@ -6,11 +6,50 @@ pub fn solve() {
         board.push(get_line());
     }
     
-
+    board_check(board);
 }
 
 fn get_line() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
     buf.trim().to_string()
+}
+
+fn board_check(lines: Vec<String>) {
+    let mut min = i32::MAX;
+    for row in 0..(lines.len() - 7) {
+        for char_index in 0..(lines[row].len() - 7) {
+            let mut v:Vec<String> = Vec::new();
+            for i in row..(row + 8) {
+                v.push(lines[i].chars().skip(char_index).take(8).collect());
+            }
+            min = min.min(lines_check(v));
+        }
+    }
+    println!("{}", min);
+}
+
+fn lines_check(lines: Vec<String>) -> i32 {
+    let w = "WBWBWBWB";
+    let b = "BWBWBWBW";
+    let start_w = format!("{}{}{}{}{}{}{}{}", w, b, w, b, w, b, w, b);
+    let start_b = format!("{}{}{}{}{}{}{}{}", b, w, b, w, b, w, b, w);
+
+
+    let line = lines.join("");
+
+    let mut counter_w = 0;
+    for (c, r) in line.chars().zip(start_w.chars()) {
+        if c != r {
+            counter_w += 1;
+        }
+    }
+    let mut counter_b = 0;
+    for (c, r) in line.chars().zip(start_b.chars()) {
+        if c != r {
+            counter_b += 1;
+        }
+    }
+
+    counter_w.min(counter_b)
 }
