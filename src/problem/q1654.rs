1adb437c0c0cb52a71135969d7f6642a88decd3d
4BMtolobPL <k4qe6urml3@gmail.com>
2022-07-18 17:30:35 +0900

    Q1654 solved
    2022_07_18 17:30 acmicpc Q1654 Solved

---
+++ src/problem/q1654.rs
@@ -0,0 +1,35 @@
+use std::io::prelude::*;
+// use std::{thread, time};
+
+pub fn solve() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    let lines: Vec<&str> = buf.split_whitespace().collect();
+    let n: u64 = lines[1].parse().unwrap();
+    let mut nums: Vec<u64> = Vec::new();
+    for line in lines.iter().skip(2) {
+        nums.push(line.parse().unwrap());
+    }
+    
+    let sum: u64 = nums.iter().sum();
+    let mut lhs: u64 = 1;
+    let mut rhs: u64 = sum / n;
+    while lhs <= rhs {
+        let middle = (rhs + lhs) / 2;
+        
+        let mut count = 0;
+        for num in &nums {
+            count += num / middle;
+        }
+
+        if count >= n {
+            lhs = middle + 1;
+        } else if count < n {
+            rhs = middle - 1;
+        }
+
+        // println!("lhs: {}, rhs: {}", lhs, rhs);
+        // thread::sleep(time::Duration::from_millis(500));
+    }
+    println!("{}", rhs);
+}
