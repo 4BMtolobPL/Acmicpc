dfd92206605d2d5fe05fb4d09dfe0344f306e510
4BMtolobPL <k4qe6urml3@gmail.com>
2022-07-21 17:43:51 +0900

    Q1978 solved
    2022_07_21 Q1978 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod problem;
 
 fn main() {
-    problem::q1966::solve();
+    problem::q1978::solve();
 }
---
+++ src/problem/q1978.rs
@@ -0,0 +1,30 @@
+use std::{io::{Read, stdin}, collections::HashSet};
+
+pub fn solve() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    let v: Vec<usize> = buf.split_whitespace().skip(1).map(|x| x.parse().unwrap()).collect();
+    let primes = get_primes(1000);
+
+    println!("{}", v.iter().filter(|x| primes.contains(*x)).count());
+}
+
+fn get_primes(rhs: usize) -> HashSet<usize> {
+    let mut nums = vec![true; rhs + 1];
+    for i in 2..=32 {
+        if nums[i] {
+            for j in ((i * i)..=rhs).step_by(i) {
+                nums[j] = false;
+            }
+        }
+    }
+
+    let mut primes = HashSet::new();
+    for (index, value) in nums.iter().enumerate().skip(2) {
+        if *value {
+            primes.insert(index);
+        }
+    }
+    primes
+}
