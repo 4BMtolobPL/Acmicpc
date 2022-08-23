16d1e4f4d60d168e6d9763385409d89b903752cc
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2022-08-23 17:55:42 +0900

    Q2275, Q10989 Solved
    2022_08_23 17:55 Q2275, Q10989 Solved

---
+++ src/problem/q10989.rs
@@ -0,0 +1,24 @@
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[allow(dead_code)]
+pub fn solve() {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    let n = buf.trim().parse().unwrap();
+
+    let mut v = [0; 10001];
+    for _ in 0..n {
+        buf.clear();
+        stdin().read_line(&mut buf).unwrap();
+        let index: usize = buf.trim().parse().unwrap();
+        v[index] += 1;
+    }
+
+    let stdout = stdout();
+    let mut out = BufWriter::new(stdout.lock());
+    for i in 0..10001 {
+        for _ in 0..v[i] {
+            writeln!(out, "{}", i).unwrap();
+        }
+    }
+}
